e88761312598d806ba23fc07916d32cc7803c185
CettleK <ghostlyninjatroll@gmail.com>
2025-02-27 16:01:05 -0800

    Quick fix

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,10 @@
 import { Link, useLocation } from "react-router-dom";
+import homeIcon from "../assets/home.png";
+import passportIcon from "../assets/passport.png";
+import trackerIcon from "../assets/tracker.png";
+import communityIcon from "../assets/community.png";
+import settingsIcon from "../assets/setting.png";
+import loginIcon from "../assets/login.png";
 
 const Sidebar = ({ isAuthenticated, handleLogout }) => {
 
@@ -11,24 +17,24 @@ const Sidebar = ({ isAuthenticated, handleLogout }) => {
         <div className="mb-10 text-xl font-bold">Health App</div>
         
         <nav className="space-y-4">
-            <Link to="/"><button className="flex items-center  w-full p-3 bg-blue-300 rounded-lg hover:bg-red-600 transition-all duration-200 text-black"><img src="/home.png" alt="Tracker Icon" className="w-6 h-6" /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home</button></Link><br></br>
-            <Link to="/Passport"><button className="flex items-center w-full p-3 bg-red-500 rounded-lg hover:bg-red-600 transition-all duration-200 text-black"><img src="/passport.png" alt="Tracker Icon" className="w-6 h-6" />&nbsp;&nbsp;&nbsp; Passport</button></Link><br></br>
-            <Link to="/Tracker"><button className="flex items-center w-full p-3 bg-red-500 rounded-lg hover:bg-red-600 transition-all duration-200 text-black"><img src="/tracker.png" alt="Tracker Icon" className="w-6 h-6" />&nbsp;&nbsp;&nbsp;&nbsp; Tracker</button></Link><br></br>
-            <Link to="/Community"><button className="flex items-center w-full p-3 bg-red-500 rounded-lg hover:bg-red-600 transition-all duration-200 text-black"><img src="/community.png" alt="Tracker Icon" className="w-6 h-6" /> Community</button></Link><br></br>
-            <Link to="/Settings"><button className="flex items-center w-full p-3 bg-red-500 rounded-lg hover:bg-red-600 transition-all duration-200 text-black"><img src="/setting.png" alt="Tracker Icon" className="w-6 h-6" />&nbsp;&nbsp;&nbsp; Settings</button></Link><br></br>
+            <Link to="/"><button className="flex items-center  w-full p-3 bg-blue-300 rounded-lg hover:bg-red-600 transition-all duration-200 text-black"><img src={homeIcon} alt="Tracker Icon" className="w-6 h-6" /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home</button></Link><br></br>
+            <Link to="/Passport"><button className="flex items-center w-full p-3 bg-red-500 rounded-lg hover:bg-red-600 transition-all duration-200 text-black"><img src={passportIcon} alt="Tracker Icon" className="w-6 h-6" />&nbsp;&nbsp;&nbsp; Passport</button></Link><br></br>
+            <Link to="/Tracker"><button className="flex items-center w-full p-3 bg-red-500 rounded-lg hover:bg-red-600 transition-all duration-200 text-black"><img src={trackerIcon} alt="Tracker Icon" className="w-6 h-6" />&nbsp;&nbsp;&nbsp;&nbsp; Tracker</button></Link><br></br>
+            <Link to="/Community"><button className="flex items-center w-full p-3 bg-red-500 rounded-lg hover:bg-red-600 transition-all duration-200 text-black"><img src={communityIcon} alt="Tracker Icon" className="w-6 h-6" />&nbsp; Community</button></Link><br></br>
+            <Link to="/Settings"><button className="flex items-center w-full p-3 bg-red-500 rounded-lg hover:bg-red-600 transition-all duration-200 text-black"><img src={settingsIcon} alt="Tracker Icon" className="w-6 h-6" />&nbsp;&nbsp;&nbsp; Settings</button></Link><br></br>
         </nav>
         
         <div className="mt-auto">
             {isAuthenticated ? (
                 <Link to="/login">
                     <button className="flex items-center w-full p-3 bg-red-500 rounded-lg hover:bg-red-600 transition-all duration-200 text-black" onClick={handleLogout}>
-                    <img src="/login.png" alt="Tracker Icon" className="w-6 h-6" /> &nbsp;&nbsp;&nbsp;&nbsp;Logout
+                    <img src={loginIcon} alt="Tracker Icon" className="w-6 h-6" /> &nbsp;&nbsp;&nbsp;&nbsp;Logout
                     </button>
                 </Link>
             ) : (
                 <Link to="/login">
                     <button className="flex items-center w-full p-3 bg-blue-500 rounded-lg hover:bg-blue-600 transition-all duration-200 text-black">
-                    <img src="/login.png" alt="Tracker Icon" className="w-6 h-6" /> &nbsp;&nbsp;&nbsp;&nbsp;Login
+                    <img src={loginIcon} alt="Tracker Icon" className="w-6 h-6" /> &nbsp;&nbsp;&nbsp;&nbsp;Login
                     </button>
                 </Link>
             )}
